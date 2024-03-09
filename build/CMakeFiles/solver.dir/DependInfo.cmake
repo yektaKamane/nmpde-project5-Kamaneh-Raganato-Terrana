c474da7ffae66f95946b33ab7d39769aa79cb4de
yektaKamane <ykamaneh@gmail.com>
2024-03-09 10:04:46 +0000

    starting point on 2 dimensional

---
+++ build/CMakeFiles/solver.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yekta/platform/nmpde-project5-Kamaneh-Raganato-Terrana/src/Fisher_Kolmogorov_solver.cpp" "CMakeFiles/solver.dir/src/Fisher_Kolmogorov_solver.cpp.o" "gcc" "CMakeFiles/solver.dir/src/Fisher_Kolmogorov_solver.cpp.o.d"
+  "/home/yekta/platform/nmpde-project5-Kamaneh-Raganato-Terrana/src/main.cpp" "CMakeFiles/solver.dir/src/main.cpp.o" "gcc" "CMakeFiles/solver.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "Fisher_Kolmogorov_solver.hpp"
+
+// Main function.
+int
+main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+
+  const unsigned int degree = 1;
+
+  const double T      = 1.0;
+  const double deltat = 0.05;
+
+  FisherKol problem("../mesh/mesh-square-20.msh", degree, T, deltat);
+
+  problem.setup();
+  problem.solve();
+
+  return 0;
+}
