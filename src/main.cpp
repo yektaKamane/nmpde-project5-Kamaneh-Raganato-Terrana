41de35edf7b9f163203ea3de746b479a4a65e815
yektaKamane <ykamaneh@gmail.com>
2024-05-21 14:58:45 +0000

    command line options

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <deal.II/base/convergence_table.h>
-
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -7,17 +5,66 @@
 #include "Fisher_Kolmogorov_solver.hpp"
 
 // Main function.
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const unsigned int dim = 2;
+  // Check if the correct number of command-line arguments is provided
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << argv[0] << " <dimension> <mesh_file> [parameter_file]" << std::endl;
+    return 1;
+  }
 
-  FisherKol<dim> problem("../mesh/mesh-square-5.msh", "../input/test1.prm");
+  // Read the mandatory dimension from command line arguments and convert to unsigned int
+  unsigned int dim;
+  try
+  {
+    dim = std::stoul(argv[1]);
+  }
+  catch (const std::invalid_argument &)
+  {
+    std::cerr << "Invalid dimension: " << argv[1] << ". Must be a positive integer." << std::endl;
+    return 1;
+  }
+  catch (const std::out_of_range &)
+  {
+    std::cerr << "Dimension out of range: " << argv[1] << std::endl;
+    return 1;
+  }
 
-  problem.setup();
-  problem.solve();
+   // Read the mandatory mesh file name from command line arguments
+  std::string mesh_file = argv[2];
+
+  // Read the optional parameter file name or use a default value
+  std::string parameter_file;
+  if (argc >= 4)
+  {
+    parameter_file = argv[3];
+  }
+  else
+  {
+    parameter_file = "../input/test1.prm"; // Default parameter file
+  }
+
+  // Instantiate the problem with the specified dimension
+  if (dim == 2)
+  {
+    FisherKol<2> problem(mesh_file, parameter_file);
+    problem.setup();
+    problem.solve();
+  }
+  else if (dim == 3)
+  {
+    FisherKol<3> problem(mesh_file, parameter_file);
+    problem.setup();
+    problem.solve();
+  }
+  else
+  {
+    std::cerr << "Unsupported dimension: " << dim << ". Only 2 and 3 are supported." << std::endl;
+    return 1;
+  }
 
 
   return 0;
