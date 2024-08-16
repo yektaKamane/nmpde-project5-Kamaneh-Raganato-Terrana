906f298c9c371261044a1170af9e3e1c2718b93d
elisa <elisa.terrana@mail.polimi.it>
2024-08-16 16:20:50 +0000

    Merging manually from branch elisa to main branch

---
+++ src/FisherKolmogorov1D_convergence.hpp
@@ -0,0 +1,343 @@
+#ifndef FISHER_KOLMOGOROV_HPP
+#define FISHER_KOLMOGOROV_HPP
+
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/distributed/fully_distributed_tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_values_extractors.h>
+#include <deal.II/fe/mapping_fe.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_in.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+
+// Class representing the non-linear diffusion problem.
+template <int dim>
+class FisherKol
+{
+public:
+  // Physical dimension (1D, 2D, 3D)
+  // static constexpr unsigned int dim = 1; // MODIFIED
+
+  // Function for the alpha coefficient.
+  // class FunctionAlpha : public Function<dim>
+  // {
+  // public:
+  //   virtual double
+  //   value(const Point<dim> & /*p*/,
+  //         const unsigned int /*component*/ = 0) const override
+  //   {
+  //     return 2.0;
+  //   }
+  // };
+  
+  // Function of the fiber field
+  class FunctionN
+  {
+  public:
+    Tensor<2, dim>
+    isotropic(const Point<dim> & /*p*/) const
+    {
+      Tensor<1, dim> n;
+      for (unsigned int i = 0; i < dim; ++i)
+        n[i] = 0.0;
+      return outer_product(n, n);
+    }
+  };
+
+  // // Function of the matrix D
+  // class FunctionD
+  // {
+  // public:
+  //   Tensor<2, dim> matrix_value(const Point<dim> & /*p*/ /* ,
+  //                  Tensor<2,dim> &values */) const
+  //   {
+  //     Tensor<2, dim> values;
+  //     for (unsigned int i = 0; i < dim; ++i)
+  //     {
+  //       values[i][i] = 0.0002;
+  //     }
+  //     // values[1][1] += 10.0;
+  //     return values;
+  //   }
+  // };
+
+  // Function for the forcing term.
+  class ForcingTerm : public Function<dim>
+  {
+  public:
+    virtual double
+    value(const Point<dim> & p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      double temp_val = std::cos(M_PI * p[0]);
+      return (M_PI * M_PI - 2) * temp_val + temp_val * temp_val;
+    }
+  };
+
+  // Function for Dirichlet boundary conditions.
+  // class FunctionG : public Function<dim>
+  // {
+  // public:
+  //   virtual double
+  //   value(const Point<dim> & /*p*/,
+  //         const unsigned int /*component*/ = 0) const override
+  //   {
+  //     return 0.0;
+  //   }
+  // };
+
+  // Function for Neumann boundary condition.
+  class FunctionH : public Function<dim>
+  {
+  public:
+    // Constructor.
+    FunctionH()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> & /*p*/,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return 0.0;
+    }
+  };
+
+  // Function for initial conditions.
+  class FunctionU0 : public Function<dim>
+  {
+  public:
+    virtual double
+    value(const Point<dim> & p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      // if (p[0] < 0.55 && p[0] > 0.45 && p[1] < 0.55 && p[1] > 0.45 && p[2] < 0.55 && p[2] > 0.45)
+      // // if (p[0] < 0.05 && p[0] > -0.05 && p[1] < 0.05 && p[1] > -0.05 && p[2] < 0.05 && p[2] > -0.05)
+      // // if(p[0] == 0)
+      // {
+      //   return 0.1;
+      // }
+      
+      // if (p[0] < 80.0 && p[0] > 70.0 && p[1] < 95.0 && p[1] > 90.0 /*&& p[2] < 50.0 && p[2] > 40.0*/)
+      // if (p[0] < 0.55 && p[0] > 0.45 && p[1] < 0.55 && p[1] > 0.45)
+      // {
+      //   return 0.3;
+      // }
+      // if (p[0]>=0.45 && p[0]<=0.55)
+      //   return 0.1;
+      return std::cos(M_PI * p[0]);
+  
+      // return 0.0;
+      // return p[0] * (1 - p[0]) * p[1] * (1 - p[1]);
+    }
+  };
+
+  // Exact solution.
+  class ExactSolution : public Function<dim>
+  {
+  public:
+    virtual double
+    value(const Point<dim> &p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return std::cos(M_PI*p[0]) * std::exp(-this->get_time());
+    }
+
+    virtual Tensor<1, dim>
+    gradient(const Point<dim> &p,
+             const unsigned int /*component*/ = 0) const override
+    {
+      Tensor<1, dim> result;
+
+      // duex / dx
+      result[0] = -M_PI * std::sin(M_PI * p[0]) * std::exp(-this->get_time());
+
+      return result;
+    }
+  };
+
+  // Constructor. We provide the final time, time step Delta t and theta method
+  // parameter as constructor arguments.
+  FisherKol(const unsigned int N_,
+                // const unsigned int &r_,
+                // const double       &T_,
+                // const double       &deltat_,
+                const std::string  &prm_file_)
+    : mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD))
+    , mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD))
+    , pcout(std::cout, mpi_rank == 0)
+    // , T(T_)
+    , N(N_)
+    // , r(r_)
+    // , deltat(deltat_)
+    , prm_file(prm_file_)
+    , mesh(MPI_COMM_WORLD)
+  {
+      parameters.declare_entry("coef_alpha", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("coef_dext", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("coef_daxn", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("fib", "0", Patterns::Integer(), "dummy");
+
+      parameters.declare_entry("T", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("deltat", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("degree", "0", Patterns::Integer(), "dummy");
+
+      parameters.parse_input(prm_file);
+  }
+
+  // Initialization.
+  void
+  setup();
+
+  // Solve the problem.
+  void
+  solve();
+
+  // Compute the error for convergence analysis.
+  double
+  compute_error(const VectorTools::NormType &norm_type);
+
+protected:
+  // Assemble the tangent problem.
+  void
+  assemble_system();
+
+  // Solve the linear system associated to the tangent problem.
+  void
+  solve_linear_system();
+
+  // Solve the problem for one time step using Newton's method.
+  void
+  solve_newton();
+
+  // Output.
+  void
+  output(const unsigned int &time_step) const;
+
+  // MPI parallel. /////////////////////////////////////////////////////////////
+
+  // Number of MPI processes.
+  const unsigned int mpi_size;
+
+  // This MPI process.
+  const unsigned int mpi_rank;
+
+  // Parallel output stream.
+  ConditionalOStream pcout;
+
+  // Problem definition. ///////////////////////////////////////////////////////
+
+  // alpha coefficient.
+  // FunctionAlpha alpha;
+
+  // ...
+  FunctionN fiber;
+
+  // matrix D.
+  // FunctionD D;
+
+  // Forcing term.
+  ForcingTerm forcing_term;
+
+  // Dirichlet boundary conditions.
+  // FunctionG function_g;
+
+  // Neumann boundary condition.
+  FunctionH function_h;
+
+  // Initial conditions.
+  FunctionU0 u_0;
+
+  // Exact solution.
+  ExactSolution exact_solution;
+
+  // Current time.
+  double time;
+
+  // Final time.
+  // const double T;
+
+  // Number of elements.
+  const unsigned int N;
+
+  // Discretization. ///////////////////////////////////////////////////////////
+
+  // Mesh file name.
+  // const std::string mesh_file_name;
+
+  // Polynomial degree.
+  // const unsigned int r;
+
+  // Time step.
+  // const double deltat;
+
+  const std::string prm_file;
+
+  ParameterHandler parameters;
+
+  // Mesh.
+  parallel::fullydistributed::Triangulation<dim> mesh;
+  // Triangulation<dim> mesh;
+
+  // Finite element space.
+  std::unique_ptr<FiniteElement<dim>> fe;
+
+  // Quadrature formula.
+  std::unique_ptr<Quadrature<dim>> quadrature;
+
+  // Quadrature formula used on boundary lines.
+  std::unique_ptr<Quadrature<dim - 1>> quadrature_boundary;
+
+  // DoF handler.
+  DoFHandler<dim> dof_handler;
+
+  // DoFs owned by current process.
+  IndexSet locally_owned_dofs;
+
+  // DoFs relevant to the current process (including ghost DoFs).
+  IndexSet locally_relevant_dofs;
+
+  // Jacobian matrix.
+  TrilinosWrappers::SparseMatrix jacobian_matrix;
+
+  // Residual vector.
+  TrilinosWrappers::MPI::Vector residual_vector;
+
+  // Increment of the solution between Newton iterations.
+  TrilinosWrappers::MPI::Vector delta_owned;
+
+  // System solution (without ghost elements).
+  TrilinosWrappers::MPI::Vector solution_owned;
+
+  // System solution (including ghost elements).
+  TrilinosWrappers::MPI::Vector solution;
+
+  // System solution at previous time step.
+  TrilinosWrappers::MPI::Vector solution_old;
+};
+
+#endif
---
+++ src/Fisher_Kolmogorov_solver_convergence.hpp
@@ -0,0 +1,453 @@
+#ifndef FISHER_KOLMOGOROV_HPP
+#define FISHER_KOLMOGOROV_HPP
+
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/distributed/fully_distributed_tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_simplex_p.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_values_extractors.h>
+#include <deal.II/fe/mapping_fe.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/distributed/grid_refinement.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h> // ...
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+
+// #include <deal.II/multigrid/multigrid.h> // ...
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <deal.II/base/parameter_handler.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+
+// Class representing the non-linear diffusion problem.
+template <int dim>
+class FisherKol
+{
+public:
+  // Function for the alpha coefficient.
+  // class FunctionAlpha : public Function<dim>
+  // {
+  // public:
+  //   virtual double
+  //   value(const Point<dim> & /*p*/,
+  //         const unsigned int /*component*/ = 0) const override
+  //   {
+  //     if (dim == 2) {
+  //       return 1.0;
+  //     }
+
+  //     if (dim == 3) {
+  //       return 0.1;
+  //     }
+  //   }
+  // };
+  
+  // Function of the fiber field
+  class FunctionN
+  {
+  public:
+    Tensor<2, dim>
+    isotropic(const Point<dim> & /*p*/) const
+    {
+      // Tensor<2, dim> values;
+      // for (unsigned int i = 0; i < dim; ++i)
+      //   values[i][i] = 0.0;
+      // return values;
+
+      Tensor<1, dim> n;
+      for (unsigned int i = 0; i < dim; ++i)
+        n[i] = 0.0;
+      return outer_product(n, n);
+    }
+
+    Tensor<2, dim>
+    anisotropic_x(const Point<dim> & /*p*/) const
+    {
+      // Tensor<2, dim> values;
+      // for (unsigned int i = 0; i < dim; ++i)
+      //   values[i][i] = 0.0;
+      // return values;
+
+      Tensor<1, dim> n;
+      n[0] = 1.0;
+      n[1] = 0.0;
+      return outer_product(n, n); // {{1, 0}, {0, 0}}
+    }
+
+    Tensor<2, dim>
+    anisotropic_y(const Point<dim> & /*p*/) const
+    {
+      // Tensor<2, dim> values;
+      // for (unsigned int i = 0; i < dim; ++i)
+      //   values[i][i] = 0.0;
+      // return values;
+
+      Tensor<1, dim> n;
+      n[0] = 0.0;
+      n[1] = 1.0;
+      return outer_product(n, n); // {{0, 0}, {0, 1}}
+    }
+
+    Tensor<2, dim>
+    circumferential(const Point<dim> & p) const
+    {
+      Tensor<1, dim> n;
+      // x = rho * cos(theta), y = rho * sin(theta)
+      double rho = std::sqrt(p[0]*p[0] + p[1]*p[1]);
+      double theta = std::atan2(p[1], p[0]);
+      n[0] = -std::sin(theta);
+      n[1] = std::cos(theta);
+      // n[0] = rho * std::cos(theta);
+      // n[1] = rho * std::sin(theta);
+      return outer_product(n, n);
+    }
+
+    Tensor<2, dim>
+    radial(const Point<dim> & p) const
+    {
+      Tensor<2, dim> values;
+      Tensor<1, dim> n;
+      for (unsigned int i = 0; i < dim; ++i)
+      {
+        values[i][i] = 0.0;
+      }
+      return values;
+    }
+
+    Tensor<2, dim>
+    axon_based(const Point<dim> & p) const
+    {
+      Tensor<2, dim> values;
+      Tensor<1, dim> n;
+      for (unsigned int i = 0; i < dim; ++i)
+      {
+        values[i][i] = 0.0;
+      }
+      return values;
+    }
+  };
+
+  // Function of the matrix D
+  // class FunctionD
+  // {
+  // public:
+  //   Tensor<2, dim> matrix_value(const Point<dim> & /*p ,
+  //                  Tensor<2,dim> &values */) const
+  //   {
+  //     Tensor<2, dim> values;
+  //     for (unsigned int i = 0; i < dim; ++i)
+  //     {
+  //       values[i][i] = 1.0;
+  //     }
+  //     // values[1][1] += 10.0;
+  //     return values;
+  //   }
+  // };
+
+  // Function for the forcing term.
+  class ForcingTerm : public Function<dim>
+  {
+  public:
+    virtual double
+    value(const Point<dim> & p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      // Forcing term for the convergence test.
+      double temp_val = std::cos(M_PI * p[0]) * std::cos(M_PI * p[1]);
+
+      if (dim == 2)
+        return -2 * M_PI * M_PI * temp_val * std::exp(-this->get_time()) -
+               (temp_val * temp_val - 4 * temp_val + 4) * std::exp(-this->get_time() * 2);
+
+      if (dim == 3)
+      {
+        temp_val = temp_val * std::cos(M_PI * p[2]);
+        return 0.1 * temp_val * temp_val * std::exp(-this->get_time() * 2) +
+               (3 * M_PI * M_PI - 1.1) * temp_val * std::exp(-this->get_time());
+      }
+
+      else return 0.0;
+
+      // Forcing term for exploring the behavior in the axonal direction.
+      // return 0.0;
+    }
+  };
+
+  // Function for Neumann boundary condition.
+  class FunctionH : public Function<dim>
+  {
+  public:
+    // Constructor.
+    FunctionH()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> & /*p*/,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return 0.0;
+    }
+  };
+
+  // Function for initial conditions.
+  class FunctionU0 : public Function<dim>
+  {
+  public:
+    virtual double
+    value(const Point<dim> & p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      // Initial condition for the convergence test.
+      // if (dim == 2)
+      //   return std::cos(M_PI * p[0]) * std::cos(M_PI * p[1]) + 2;
+
+      // if (dim == 3)
+      //   return std::cos(M_PI * p[0]) * std::cos(M_PI * p[1]) * std::cos(M_PI * p[2]);
+
+      // Initial condition for exploring the behavior in the axonal direction.
+      if (dim == 2)
+      {
+        if (p[0] < 0.55 && p[0] > 0.45 && p[1] < 0.55 && p[1] > 0.45)
+          return 0.1;
+      }
+
+      if (dim == 3)
+      {
+        // Use this initial condition with the brain mesh.
+        // if (p[0] < 80.0 && p[0] > 70.0 && p[1] < 95.0 && p[1] > 90.0 && p[2] < 50.0 && p[2] > 40.0)
+        //   return 0.95;
+
+        // Use this initial condition with the cube mesh.
+        if (p[0] < 0.55 && p[0] > 0.45 && p[1] < 0.55 && p[1] > 0.45 && p[2] < 0.55 && p[2] > 0.45)
+          return 0.1;
+      }
+
+      return 0.0;
+    }
+  };
+
+   // Exact solution
+  class ExactSolution : public Function<dim>
+  {
+  public:
+    virtual double
+    value(const Point<dim> &p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      double temp_val = std::cos(M_PI * p[0]) * std::cos(M_PI * p[1]);
+      if (dim == 2)
+        return (temp_val + 2) * std::exp(-this->get_time());
+
+      if (dim == 3)
+        return temp_val * std::cos(M_PI * p[2]) * std::exp(-this->get_time());
+
+      else return 0.0;
+      
+    }
+
+    virtual Tensor<1, dim>
+    gradient(const Point<dim> &p,
+             const unsigned int /*component*/ = 0) const override
+    {
+      Tensor<1, dim> result;
+
+      if (dim == 2)
+      {
+        result[0] = -M_PI * std::sin(M_PI * p[0]) * std::cos(M_PI * p[1]) *
+                    std::exp(-this->get_time());
+        result[1] = -M_PI * std::cos(M_PI * p[0]) * std::sin(M_PI * p[1]) *
+                    std::exp(-this->get_time());
+      }
+      
+      if (dim == 3)
+      {
+        result[0] = -M_PI * std::sin(M_PI * p[0]) * std::cos(M_PI * p[1]) *
+                    std::cos(M_PI * p[2]) * std::exp(-this->get_time());
+        result[1] = -M_PI * std::sin(M_PI * p[1]) * std::cos(M_PI * p[0]) *
+                    std::cos(M_PI * p[2]) * std::exp(-this->get_time());
+        result[2] = -M_PI * std::sin(M_PI * p[2]) * std::cos(M_PI * p[0]) *
+                    std::cos(M_PI * p[1]) * std::exp(-this->get_time());
+      }
+
+      return result;
+    }
+  };
+
+  // Constructor. We provide the final time, time step Delta t and theta method
+  // parameter as constructor arguments.
+  FisherKol(const std::string  &mesh_file_name_,
+                // const unsigned int &r_,
+                // const double       &T_,
+                // const double       &deltat_,
+                const std::string  &prm_file_)
+    : mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD))
+    , mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD))
+    , pcout(std::cout, mpi_rank == 0)
+    // , T(T_)
+    , mesh_file_name(mesh_file_name_)
+    // , r(r_)
+    // , deltat(deltat_)
+    , prm_file(prm_file_)
+    , mesh(MPI_COMM_WORLD)
+  {
+      parameters.declare_entry("coef_alpha", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("coef_dext", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("coef_daxn", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("fib", "0", Patterns::Integer(), "dummy");
+
+      parameters.declare_entry("T", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("deltat", "0.0", Patterns::Double(), "dummy");
+      parameters.declare_entry("degree", "0", Patterns::Integer(), "dummy");
+
+      parameters.parse_input(prm_file);
+  }
+
+  // Initialization.
+  void
+  setup();
+
+  // Solve the problem.
+  void
+  solve();
+
+  // Compute the error for convergence analysis.
+  double
+  compute_error(const VectorTools::NormType &norm_type);
+
+protected:
+  // Assemble the tangent problem.
+  void
+  assemble_system();
+
+  // Solve the linear system associated to the tangent problem.
+  void
+  solve_linear_system();
+
+  // Solve the problem for one time step using Newton's method.
+  void
+  solve_newton();
+
+  // Output.
+  void
+  output(const unsigned int &time_step) const;
+
+
+  // MPI parallel. /////////////////////////////////////////////////////////////
+
+  // Number of MPI processes.
+  const unsigned int mpi_size;
+
+  // This MPI process.
+  const unsigned int mpi_rank;
+
+  // Parallel output stream.
+  ConditionalOStream pcout;
+
+  // Problem definition. ///////////////////////////////////////////////////////
+
+  // alpha coefficient.
+  // FunctionAlpha alpha;
+
+  // ...
+  FunctionN fiber;
+
+  // matrix D.
+  // FunctionD D;
+
+  // Forcing term.
+  ForcingTerm forcing_term;
+
+  // Neumann boundary condition.
+  FunctionH function_h;
+
+  // Initial conditions.
+  FunctionU0 u_0;
+
+  // Exact solution.
+  ExactSolution exact_solution;
+
+  // Current time.
+  double time;
+
+  // Final time.
+  // const double T;
+
+  // Discretization. ///////////////////////////////////////////////////////////
+
+  // Mesh file name.
+  const std::string mesh_file_name;
+
+  // Polynomial degree.
+  // const unsigned int r;
+
+  // Time step.
+  // const double deltat;
+
+  const std::string prm_file;
+
+  ParameterHandler parameters;
+
+  // Mesh.
+  parallel::fullydistributed::Triangulation<dim> mesh;
+
+  // Finite element space.
+  std::unique_ptr<FiniteElement<dim>> fe;
+
+  // Quadrature formula.
+  std::unique_ptr<Quadrature<dim>> quadrature;
+
+  // Quadrature formula used on boundary lines.
+  std::unique_ptr<Quadrature<dim - 1>> quadrature_boundary;
+
+  // DoF handler.
+  DoFHandler<dim> dof_handler;
+
+  // DoFs owned by current process.
+  IndexSet locally_owned_dofs;
+
+  // DoFs relevant to the current process (including ghost DoFs).
+  IndexSet locally_relevant_dofs;
+
+  // Jacobian matrix.
+  TrilinosWrappers::SparseMatrix jacobian_matrix;
+
+  // Residual vector.
+  TrilinosWrappers::MPI::Vector residual_vector;
+
+  // Increment of the solution between Newton iterations.
+  TrilinosWrappers::MPI::Vector delta_owned;
+
+  // System solution (without ghost elements).
+  TrilinosWrappers::MPI::Vector solution_owned;
+
+  // System solution (including ghost elements).
+  TrilinosWrappers::MPI::Vector solution;
+
+  // System solution at previous time step.
+  TrilinosWrappers::MPI::Vector solution_old;
+};
+
+#endif
